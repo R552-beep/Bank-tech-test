9a57f7543f7835c3bd20d9df45bf8493fd176813
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-08-23 22:07:06 +0100

    Created tests for transaction log class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,4 @@
+
 require 'account'
 
 describe Account do
--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -1,12 +1,33 @@
+
 require 'transaction_log'
 
 describe Transaction_log do
-  
-  # it 'is displays the date of the transaction' do
-  #   expeect(subject.display).to eq "#{Time.new.strftime("%d/%m/%y)} || || || "
-  # end  
-  
-  
+
+  context "#display" do 
+    it 'logs and shows the date of the transaction' do
+      expect(transaction_log.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || ||  ||  "
+    end
+
+    it ' shows the amount withdrawn from the account' do
+      debit_transaction = Transaction_log.new(debit: 10.0)
+      expect(debit_transaction.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || || 10.00||  "
+    end
+
+    it ' show the amount deposited to the account' do
+      credit_transaction = Transaction_log.new(credit: 20.0)
+      expect(credit_transaction.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || 20.00|| ||  "
+    end
 
 
-end
+
+  end
+  
+end    # it ' show the balance in the bank statement' do
+#   balance_of_statment = Transaction_log.new(balance: 30.0)
+#   expect(balance_of_statment.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || || || 30.00 "
+# end
+
+# it ' show full transaction of the bank statement' do
+#   statement = Transaction_log.new(credit: 5.0, debit: 15.0, balance: 70.0)
+#   expect(statement.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || 5.00|| 15.00|| 70.00 "
+# end
